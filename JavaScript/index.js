1f96e75db3e71d8fefcee76b82756e262c314980
imfelipegomes <felipe.agomesdelima@gmail.com>
2024-12-30 23:25:32 -0300

    Estudando e praticando Javascript

---
+++ JavaScript/index.js
@@ -0,0 +1,29 @@
+
+var nomeDoAluno = 'Felipe Alves Gomes de Lima' // String é um conteudo em texto entre "" ou '' --- Utilize sempre camelCase //
+var idade = '27' // Number //
+var altura = '1.81'
+var estudando = true // boolean - verdadeiro ou falso //
+
+console.log(nomeDoAluno, typeof nomeDoAluno);
+console.log(idade, typeof idade);
+console.log(altura, typeof altura);
+console.log(estudando, typeof estudando);
+
+var semConteudo; // declarando uma variável //
+
+console.log(semConteudo);
+
+var curso = 'Front-end em React',
+    topico = 'JavaScript Básico I';
+
+    console.log(curso, topico);
+
+    //! NÃO utilize mais o var em seus códigos de JavaScript, use let ou const //
+    // var é uma variável global, antes mesmo de ser declarada, ela já existe no código //
+
+let notaDoAluno = 10;
+const mediaAluno = 8;
+
+console.log(notaDoAluno);
+console.log(mediaDoAluno);
+
